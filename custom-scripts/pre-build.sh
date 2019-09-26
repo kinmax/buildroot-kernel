3012506abe1ed83e2da59ee7d5954afff153b45e
Laboratório de Redes <labredes@novoHost.portoalegre.pucrsnet.br>
2019-09-26 09:31:24 -0300

    disk tests

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -9,3 +9,8 @@ COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
 $COMPILER -o $BUILDROOT_DIR/output/target/bin/sleeping_procs $BUILDROOT_DIR/custom-scripts/sleeping_procs.c
 
 make -C $BASE_DIR/../modules/simple_driver/
+
+#Compile the hello write to disk
+BUILDROOT_DIR=$BASE_DIR/..
+COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
+$COMPILER -o $BUILDROOT_DIR/output/target/usr/bin/hello_world $BUILDROOT_DIR/disk-test/hello_world.c
