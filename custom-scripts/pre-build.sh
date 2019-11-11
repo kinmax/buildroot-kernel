7e52ecf7a13bb1ee82db011ac4e83b8ce7c09a16
Kin Gusmão <gusmaokin@gmail.com>
2019-11-11 02:57:08 -0300

    avancos

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -15,6 +15,8 @@ BUILDROOT_DIR=$BASE_DIR/..
 COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
 $COMPILER -o $BUILDROOT_DIR/output/target/usr/bin/hello_world $BUILDROOT_DIR/disk-test/hello_world.c
 
+$COMPILER -o $BUILDROOT_DIR/output/target/usr/bin/thread_runner $BUILDROOT_DIR/custom_scripts/thread_runner.c
+
 cp $BASE_DIR/../custom-scripts/fstab $BUILDROOT_DIR/output/target/etc/fstab
 
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
--- custom-scripts/thread_runner.c
+++ custom-scripts/thread_runner.c
@@ -7,6 +7,46 @@
 #include <linux/sched.h>
 
 char* buffer;
+int current_size, buffer_size, number_of_threads, priority, policy;
+sem_t mutex;
+
+void *run(void *character)
+{
+	char my_char = (char)(long int)character;
+	while(1)
+	{
+	    sem_wait(&mutex);
+	    if(current_size == buffer_size)
+	    {
+	        break;
+	    }
+	    printf("%c", my_char);
+	    buffer[current_size] = my_char;
+	    current_size++;
+	    sem_post(&mutex);
+	}
+	return(NULL);
+}
+
+void setpriority(pthread_t *thr, int newpolicy, int newpriority){
+	int policy, ret;
+    struct sched_param param;
+    pthread_t t;
+    if(newpriority > sched_get_priority_max(newpolicy) ||
+	   newpriority < sched_get_priority_min(newpolicy)){
+		printf("Invalid priority: MIN: %d, MAX: %d", sched_get_priority_min(newpolicy), sched_get_priority_max(newpolicy));
+	}
+
+    pthread_getschedparam(*thr, &policy, &param);
+    
+    param.sched_priority = newpriority;
+    
+    ret = pthread_setschedparam(*thr, newpolicy, &param);
+
+    if (ret != 0){
+		perror("perror(): ");
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -16,29 +56,29 @@ int main(int argc, char **argv)
 		exit(0);
 	}
 	
-	int threads = strtol(argv[1]);
-	if(threads < 1)
+	number_of_threads = atoi(argv[1]);
+	if(number_of_threads < 1)
 	{
-		printf("The number of specified threads must bean integer greater than 0. Got %d\n\n", threads);
+		printf("The number of specified threads must be an integer greater than 0. Got %d\n\n", number_of_threads);
 		exit(0);
 	}
 	
-	int buffer_size = strtol(argv[2]);
+	buffer_size = atoi(argv[2]);
 	if(buffer_size < 1)
 	{
 		printf("The specified buffer size must be an integer greater than 0 (at least 1 kB). Got %d kB\n\n", buffer_size);
-		exit(0)
+		exit(0);
 	}
+	buffer_size = buffer_size * 1024;
 	
 	char* string_policy = argv[3];
 	
-	int priority = strtol(argv[4]);
+	priority = atoi(argv[4]);
 	if(priority < 0)
 	{
 		printf("The specified priority must be at least 0. Got %d\n\n", priority);
 	}
 	
-	int policy;
 	
 	if(strcmp(string_policy, "SCHED_FIFO") == 0)
 	{
@@ -54,8 +94,26 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		printf("Invalid scheduling policy: %s\nPolicy should be one of these: SCHED_FIFO SCHED_RR SCHED OTHER\n\n", string_policy);
+		printf("Invalid scheduling policy: %s\nPolicy should be one of these: SCHED_FIFO SCHED_RR SCHED_OTHER\n\n", string_policy);
 		exit(0);
 	}
 	
+	long int character = 65, i;
+	pthread_t threads[number_of_threads];
+	buffer = malloc(sizeof(buffer_size));
+	current_size = 0;
+	sem_init(&mutex, 0, 1);
+	for(i = 0; i < number_of_threads; i++)
+	{
+	    pthread_create(&threads[i], NULL, run, (void *)character);		
+	    setpriority(&threads[i], policy, priority);
+	    character++;
+	}
+	
+	printf("%s", buffer);
+	sem_destroy(&mutex);
+	pthread_exit(NULL);
+	return(0);
+	
 }
+
